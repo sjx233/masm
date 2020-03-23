54a2c17b8009bb208ad9be926cfdece42a1288af
undefined <sjx233@qq.com>
2020-03-23 22:32:58 +0800

    Remove progress bar
    Signed-off-by: undefined <sjx233@qq.com>

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,6 @@ import { DataPack } from "minecraft-packs";
 import { compileTo } from ".";
 import { description, name, version } from "./version";
 import commander = require("commander");
-import ProgressBar = require("progress");
 
 commander
   .name(name)
@@ -31,22 +30,11 @@ const {
   dump?: true;
 };
 
-function createProgressBar(action: string, total: number): ProgressBar {
-  return new ProgressBar("[:bar] :current/:total " + action, {
-    clear: true,
-    complete: "=",
-    incomplete: " ",
-    total,
-    width: 18
-  });
-}
-
 (async () => {
   const input = fs.readFileSync(fileName === "-" ? 0 : fs.openSync(fileName, "r"));
   const pack = new DataPack(packDescription);
   compileTo(pack, namespace, input, dump);
-  const bar = createProgressBar("writing files: :id", pack.functions.size);
-  await pack.write(output, (type, id) => type === "function" && bar.tick({ id }));
+  await pack.write(output);
 })().catch(error => {
   process.stderr.write(`unexpected error: ${error && error.stack ? error.stack : error}\n`);
   process.exit(1);
